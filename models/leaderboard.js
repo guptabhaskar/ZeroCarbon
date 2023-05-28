4c7d3813ea9f6a3658a3b003db3d4e474aa6c196
Bhaskar Gupta <guptabhanu1999@gmail.com>
2023-05-28 08:36:44 -0700

    added leaderboard

--- models/leaderboard.js
+++ models/leaderboard.js
@@ -1,14 +1,18 @@
 import { sequelize } from "./dbStart";
 import { DataTypes } from "sequelize";
 
-export const Leaderboard = sequelize.define("leaderboard", {
+export const Leaderboard = sequelize.define(
+  "leaderboard",
+  {
     user_id: {
-        type: DataTypes.STRING,
-        primaryKey: true,
+      type: DataTypes.STRING,
+      primaryKey: true,
     },
     carbon_emissions: {
-        type: DataTypes.FLOAT
+      type: DataTypes.FLOAT,
     },
-}, {
+  },
+  {
     timestamps: false,
-});
+  }
+);
--- pages/api/leaderboard.js
+++ pages/api/leaderboard.js
@@ -1,28 +1,28 @@
 import { Leaderboard } from "../../models/leaderboard";
 
 export default async function handler(req, res) {
-    if (req.method === "GET") {
-        try {
-            const leaderboard = await Leaderboard.findAll({
-                order: [["carbon_emissions", "ASC"]],
-            });
-            res.json(leaderboard);
-        } catch (error) {
-            res.status(500).json({
-                message: error.message,
-            });
-        }
-    } else if (req.method === "POST") {
-        try {
-            await Leaderboard.upsert({
-                user_id: req.body.user_id,
-                carbon_emissions: req.body.carbon_emissions,
-            });
-            res.json({ success: true });
-        } catch (error) {
-            res.status(500).json({
-                message: error.message,
-            });
-        }
+  if (req.method === "GET") {
+    try {
+      const leaderboard = await Leaderboard.findAll({
+        order: [["carbon_emissions", "ASC"]],
+      });
+      res.json(leaderboard);
+    } catch (error) {
+      res.status(500).json({
+        message: error.message,
+      });
     }
+  } else if (req.method === "POST") {
+    try {
+      await Leaderboard.upsert({
+        user_id: req.body.user_id,
+        carbon_emissions: req.body.carbon_emissions,
+      });
+      res.json({ success: true });
+    } catch (error) {
+      res.status(500).json({
+        message: error.message,
+      });
+    }
+  }
 }
